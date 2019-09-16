1c90fb38c794e019b8da4f689cc407c1a6dde577
Alison Kelly <alisonbv@gmail.com>
2019-09-15 20:46:31 -0400

    added functionality for the server and successfully set up the MVC for the app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (kind) VALUES ('Gardein Burger');
+INSERT INTO burgers (kind) VALUES ('LiteLife Burger');
+INSERT INTO burgers (kind, eaten) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (kind, eaten) VALUES ('Beyond Burger', true);
+INSERT INTO burgers (kind, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (kind) VALUES ('Tofurky Burger');
